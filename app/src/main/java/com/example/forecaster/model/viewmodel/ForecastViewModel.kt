b591a52d68c1c968c24ae1cc7454df6a3cb48f6a
fahimhoque <fhoque.arnob@gmail.com>
2022-11-10 12:32:18 +0600

    changed android viewmodel to view model

--- app/src/main/java/com/example/forecaster/model/viewmodel/ForecastViewModel.kt
+++ app/src/main/java/com/example/forecaster/model/viewmodel/ForecastViewModel.kt
@@ -3,20 +3,21 @@ package com.example.forecaster.model.viewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.forecaster.model.datamodel.ForecastResponse
 import com.example.forecaster.repository.data.ForecastRepository
 
-class ForecastViewModel(application: Application) : AndroidViewModel(application) {
+class ForecastViewModel : ViewModel() {
 
-    private var forecast_repository: ForecastRepository?=null
-    var forecast_data : MutableLiveData<ForecastResponse?>?=null
+    private var forecastRepository: ForecastRepository?=null
+    var forecastData : MutableLiveData<ForecastResponse?>?=null
 
     init {
-        forecast_repository = ForecastRepository()
-        forecast_data = MutableLiveData()
+        forecastRepository = ForecastRepository()
+        forecastData = MutableLiveData()
     }
 
     fun fetch_forecast(){
-        forecast_data = forecast_repository?.fetchForecast()
+        forecastData = forecastRepository?.fetchForecast()
     }
 }
